098f0bd051cb585bf8077fa9faad23c3eecb7bbd
Soroush Mehraban <Smehraban2013@gmail.com>
2020-03-03 22:35:17 +0330

    GUI file opener created

--- codes/GUI.py
+++ codes/GUI.py
@@ -1,19 +1,41 @@
 import tkinter as tk
+from tkinter import ttk, filedialog
 
 
 class MainWindow:
     def __init__(self, subject):
-        self.root = tk.Tk()
-
         self.subject = subject
 
-        self.label = None
+
+        self.initialize_root()
+        self.initialize_file_manager()
+        self.initialize_shortcut_label()
+
+    def initialize_shortcut_label(self):
         self.labelStringVar = tk.StringVar()
+        self.shortcut_label = tk.Label(self.root, textvariable=self.labelStringVar)
+        self.shortcut_label.grid(column=2, row=1, padx=20, pady=20)
+
+    def initialize_file_manager(self):
+        self.label_frame = ttk.LabelFrame(self.root, text="Open a sound file!")
+        self.label_frame.grid(column=1, row=1, padx=20, pady=20)
+        self.initialize_button()
+
+    def initialize_button(self):
+        self.file_button = ttk.Button(self.label_frame, text="Browse A File", command=self.file_dialog)
+        self.file_button.grid(column=1, row=1)
+
+    def file_dialog(self):
+        file_name = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=
+        (("mp3 files", "*.mp3"), ("all files", "*.*")))
+        print(file_name)
+
+    def initialize_root(self):
+        self.root = tk.Tk()
+        self.root.title("Sound effect")
+        self.root.minsize(640, 400)
 
     def start(self):
-        self.labelStringVar.set("Hi!")
-        self.label = tk.Label(self.root, textvariable=self.labelStringVar)
-        self.label.pack()
         self.root.mainloop()
 
     def update(self):
