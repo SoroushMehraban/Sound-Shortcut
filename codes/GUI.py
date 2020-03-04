0cbe8b8f008698bfd75f1b0fc81e7007889222b3
Soroush Mehraban <Smehraban2013@gmail.com>
2020-03-04 08:54:35 +0330

    Shortcut insert activates when file is selected only 2 keys should be inserted in each shortcut

--- codes/GUI.py
+++ codes/GUI.py
@@ -6,6 +6,7 @@ class MainWindow:
     def __init__(self, subject):
         self.subject = subject
         self.shortcut_keys = []
+        self.file_selected_flag = 0
 
         self.initialize_root()
         self.initialize_file_manager()
@@ -33,6 +34,7 @@ class MainWindow:
     def file_dialog(self):
         file_name = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=
         (("mp3 files", "*.mp3"), ("all files", "*.*")))
+        self.file_selected_flag = 1
         print(file_name)
 
     def initialize_root(self):
@@ -43,22 +45,36 @@ class MainWindow:
     def start(self):
         self.root.mainloop()
 
-    def update(self):
-        last_key = self.subject.get_last_key()
-
-        if last_key == 'enter':
-            self.shortcut_keys = []
-        elif last_key == 'backspace':
-            self.shortcut_keys.pop()
-        else:
-            self.shortcut_keys.append(last_key)
-
+    def __prepare_print_shortcut(self):
         if len(self.shortcut_keys) != 0:
             current = self.shortcut_keys[0]
             for key in self.shortcut_keys[1:]:
                 current += '+' + key
         else:
             current = ''
+        return current
 
-        self.labelStringVar.set("Shortcut:   " + current)
-        self.root.update_idletasks()
+    def __add_or_remove_shortcut(self, last_key):
+        if last_key == 'enter':  # if user press enter key
+            if len(self.shortcut_keys) == 2:  # and already pressed 2 keys before,save shortcut else do nothing
+                self.shortcut_keys = []
+                self.file_selected_flag = 0
+
+        elif last_key == 'backspace':  # if user press backspace key
+            if len(
+                    self.shortcut_keys) != 0:  # and already pressed 1 or 2 keys before, delete one of them else do nothing
+                self.shortcut_keys.pop()
+
+        elif len(self.shortcut_keys) < 2:  # add only 2 keys in shortcut
+            self.shortcut_keys.append(last_key)
+
+    def update(self):
+        print(self.file_selected_flag)
+        if self.file_selected_flag:
+            last_key = self.subject.get_last_key()  # get last key pressed by user
+
+            self.__add_or_remove_shortcut(last_key)
+            current = self.__prepare_print_shortcut()
+
+            self.labelStringVar.set("Shortcut:   " + current)
+            self.root.update_idletasks()
